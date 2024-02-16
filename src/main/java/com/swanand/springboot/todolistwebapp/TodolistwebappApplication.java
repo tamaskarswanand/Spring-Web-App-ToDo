583e7c3fb1709bdae94a6b43abe55b27fcab2ddd
Swanand Tamaskar <tamaskarswanand@gmail.com>
2024-02-16 13:17:23 +0530

    first commit

---
+++ src/main/java/com/swanand/springboot/todolistwebapp/TodolistwebappApplication.java
@@ -0,0 +1,13 @@
+package com.swanand.springboot.todolistwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistwebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swanand/springboot/todolistwebapp/TodolistwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swanand.springboot.todolistwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
